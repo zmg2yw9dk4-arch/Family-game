91f46075fce466883b78002d9c448d924a762e43
zmg2yw9dk4-arch <zmg2yw9dk4@privaterelay.appleid.com>
2026-09-21 18:51:22 +0200

    Add sw.js

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='familie-quest-v3';
+const CORE=['./','./index.html','./manifest.webmanifest','./icons/icon.svg'];
+self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()));});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET') return;event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(c=>c.put(event.request,copy)).catch(()=>{});return response;}).catch(()=>caches.match('./index.html'))));});
